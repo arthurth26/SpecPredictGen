ca2d322e226c0a0e3585307915e2dd23f8e19a9c
Katelyn Dickey <katedickey94@gmail.com>
2021-10-10 01:55:44 -0400

    Improve performance and add more logging

--- SpecToSheet.py
+++ SpecToSheet.py
@@ -11,6 +11,7 @@ def getSpecData(moleculeFolder: Path, moleculeName: str):
         if not path.exists():
             break
         with path.open('r') as f:
+            print(f'Parse S{i}')
             f.readline()
             yield (i, [tuple(Decimal(x) for x in l.split()[:2]) for l in f.readlines()])
             i += 1
@@ -19,6 +20,11 @@ def expf(v):
     return '{:0.6e}'.format(v)
 def floatf(v):
     return '{:0.6f}'.format(v)
+
+WL_KEY = 'WL (nm)'
+STOT_KEY = 'S(tot)'
+NORM_KEY = 'Normalized'
+SHIFT_KEY = 'Shifted'
 
 def main():
     config = ConfigParser(comment_prefixes=('#', ';'), inline_comment_prefixes=('#', ';'))
@@ -29,45 +35,52 @@ def main():
 
     shift = Decimal(config['vars']['shift'])
 
-    fieldNames = ['WL (nm)', 'S1']
+    fieldNames = [WL_KEY, 'S1']
 
     for i, spec in getSpecData(moleculeFolder, moleculeName):
         if (i == 1):
             specs = {row[0]: [row[1]] for row in spec}
-            rows = {row[0]: {'WL (nm)': floatf(row[0]), 'S1': expf(row[1])} for row in spec}
+            rows = {row[0]: {WL_KEY: floatf(row[0]), 'S1': expf(row[1])} for row in spec}
             wls = rows.keys()
             minWl = min(wls)
         else:
+            key = f'S{i}'
+            if key not in fieldNames:
+                fieldNames.append(key)
             for row in spec:
                 wl = row[0]
                 st = row[1]
-                if wl >= minWl and wl in rows:
-                    key = f'S{i}'
-                    rows[wl][key] = expf(st)
-                    specs[wl].append(st)
-                    if key not in fieldNames:
-                        fieldNames.append(key)
-                elif wl >= minWl:
-                    print(f'WARNING: Wavelength {wl} in S{i} not matched')
+                if wl >= minWl:
+                    try:
+                        rows[wl][key] = expf(st)
+                        specs[wl].append(st)
+                    except KeyError:
+                        print(f'WARNING: Wavelength {wl} in S{i} not matched to S1')
     
+
+    fieldNames.append(STOT_KEY)
+    fieldNames.append(NORM_KEY)
+    fieldNames.append(SHIFT_KEY)
+
     maxSpec = 0
     specTotals = {}
 
+    print('Calculate S(tot)')
     for wl in rows:
         specTotals[wl] = sum(specs[wl])
-        rows[wl]['S(tot)'] = expf(specTotals[wl])
+        rows[wl][STOT_KEY] = expf(specTotals[wl])
     
+    print('Calculate S(max)')
     maxSpec = max(specTotals[wl] for wl in specTotals)
 
+    print('Calculate normalized/shifted values')
     for wl in rows:
-        rows[wl]['Normalized'] = expf(specTotals[wl] / maxSpec)
-        rows[wl]['Shifted'] = floatf(wl + shift)
+        rows[wl][NORM_KEY] = expf(specTotals[wl] / maxSpec)
+        rows[wl][SHIFT_KEY] = floatf(wl + shift)
 
-    fieldNames.append('S(tot)')
-    fieldNames.append('Normalized')
-    fieldNames.append('Shifted')
-
-    with (moleculeFolder / f'{moleculeName}.csv').open('w') as csvFile:
+    outFilePath = moleculeFolder / f'{moleculeName}.csv'
+    print(f'Write {outFilePath}')
+    with (outFilePath).open('w') as csvFile:
         writer = csv.DictWriter(csvFile, fieldnames=fieldNames)
         writer.writeheader()
         writer.writerows(rows[wl] for wl in rows)
